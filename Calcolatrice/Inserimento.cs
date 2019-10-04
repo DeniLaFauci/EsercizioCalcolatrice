c078c18d33323392d17f1dd85905a05fbea6473e
DeniLaFauci <56152461+DeniLaFauci@users.noreply.github.com>
2019-10-04 12:01:13 +0200

    FirstCommitOfSourceCode
    IRìì

---
+++ Calcolatrice/Inserimento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyInserimento
+{
+    class Inserimento
+    {
+       
+        public static int insertInto() 
+        {
+            int x;
+            bool success;
+            do
+            {
+                Console.WriteLine("Inserisci il primo numero e primi invio");
+                string numero1 = Console.ReadLine();
+                success = Int32.TryParse(numero1, out x);
+                if (success == false)          //
+                {
+                    Console.WriteLine("Il valore inserito non è corretto, riprovare");
+                }
+            } while (success == false);
+            return x;
+        }
+    }
+  
+}
---
+++ Calcolatrice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MyOperazioni;
+using MyInserimento;
+
+namespace Calcolatrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Op;
+            int n1;
+            int n2;
+            int ris;
+            Console.WriteLine("BENVENUTO IN UMANA CALCULATOR! SCEGLIERE L'OPERAZIONE DESIDERATA!");
+            n1 = Inserimento.insertInto();
+            n2 = Inserimento.insertInto();
+            ris = Operazioni.InsertOperazioni(n1, n2);   //op ancora nn esiste
+            Console.WriteLine("risultato ",ris);
+            Console.ReadLine();
+        }
+    }
+}
